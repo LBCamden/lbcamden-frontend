33db52bab2e94af20758e002e629d7b057f5787f
benwatsonuk <hello@benwatson.eu>
2022-06-14 18:08:19 +0100

    Build progress

--- tasks/gulp/compile-assets.js
+++ tasks/gulp/compile-assets.js
@@ -35,11 +35,11 @@ const isDist = taskArguments.destination === 'dist' || false
 
 // Set the destination
 const destinationPath = function () {
-  // Public & Dist directories do no need namespaced with `govuk`
+  // Public & Dist directories do no need namespaced with `lbcamden`
   if (taskArguments.destination === 'dist' || taskArguments.destination === 'public') {
     return taskArguments.destination
   } else {
-    return `${taskArguments.destination}/govuk/`
+    return `${taskArguments.destination}/lbcamden/`
   }
 }
 
@@ -71,7 +71,7 @@ function compileStyles (done) {
     ])))
     .pipe(gulpif(isDist,
       rename({
-        basename: 'govuk-frontend',
+        basename: 'lbcamden-frontend',
         extname: '.min.css'
       })
     ))
@@ -110,7 +110,7 @@ function compileOldIE (done) {
     ])))
     .pipe(gulpif(isDist,
       rename({
-        basename: 'govuk-frontend-ie8',
+        basename: 'lbcamden-frontend-ie8',
         extname: '.min.css'
       })
     ))
@@ -198,10 +198,10 @@ gulp.task('js:compile', (done) => {
   srcFiles.forEach(function (file) {
     // This is combined with desinationPath in gulp.dest()
     // so the files are output to the correct folders
-    const newDirectoryPath = path.dirname(file).replace('src/govuk', '')
+    const newDirectoryPath = path.dirname(file).replace('src/lbcamden', '')
 
     // We only want to give component JavaScript a unique module name
-    let moduleName = 'GOVUKFrontend'
+    let moduleName = 'LBCamdenFrontend'
     if (path.dirname(file).includes('/components/')) {
       moduleName = componentNameToJavaScriptModuleName(path.parse(file).name)
     }
@@ -221,7 +221,7 @@ gulp.task('js:compile', (done) => {
       })))
       .pipe(gulpif(isDist,
         rename({
-          basename: 'govuk-frontend',
+          basename: 'lbcamden-frontend',
           extname: '.min.js'
         })
       ))
