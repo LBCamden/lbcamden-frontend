a93a9de58aadbcb5c27760724a0364158f034908
benwatsonuk <hello@benwatson.eu>
2022-06-20 16:23:39 +0100

    All tests passing

--- src/lbcamden/settings/colours.test.js
+++ src/lbcamden/settings/colours.test.js
@@ -1,40 +1,38 @@
 /* eslint-env jest */
 
-// const { renderSass } = require('../../../lib/jest-helpers')
-//
-// const configPaths = require('../../../config/paths.json')
-//
-// const sassConfig = {
-//   includePaths: [configPaths.src, 'node_modules/'],
-//   outputStyle: 'compressed'
-// }
+const { renderSass } = require('../../../lib/jest-helpers')
 
-// describe('Organisation colours', () => {
-// @todo - write function to check contract ratio of colours in palette
-// it('should define websafe colours that meet contrast requirements', async () => {
-//   const sass = `
-//     @import "settings/compatibility";
-//     @import "settings/colours-palette";
-//     @import "settings/colours-organisations";
-//     @import "settings/colours-applied";
-//     @import "helpers/colour";
-//
-//     @import "sass-color-helpers/stylesheets/color-helpers";
-//
-//     $minimum-contrast: 4.5;
-//
-//     @each $organisation in map-keys($govuk-colours-organisations) {
-//
-//       $colour: govuk-organisation-colour($organisation);
-//       $contrast: ch-color-contrast($govuk-body-background-colour, $colour);
-//
-//       @if ($contrast < $minimum-contrast) {
-//         @error "Contrast ratio for #{$organisation} too low."
-//         + " #{$colour} on #{$govuk-body-background-colour} has a contrast of: #{$contrast}."
-//         + " Must be higher than #{$minimum-contrast} for WCAG AA support.";
-//       }
-//     }`
-//
-//   await renderSass({ data: sass, ...sassConfig })
-// })
-// })
+const configPaths = require('../../../config/paths.json')
+
+const sassConfig = {
+  includePaths: [configPaths.src, 'node_modules/'],
+  outputStyle: 'compressed'
+}
+
+describe('Organisation colours', () => {
+  it('should define websafe colours that meet contrast requirements', async () => {
+    const sass = `
+    @import "helpers/colour";
+
+    @import "sass-color-helpers/stylesheets/color-helpers";
+
+    $minimum-contrast: 4.5;
+
+    $govuk-body-background-colour: #ffffff;
+
+    @each $brandColour in map-keys($lbcamden-colours) {
+
+      $colour: lbcamden-colour($brandColour);
+      $contrast: ch-color-contrast($govuk-body-background-colour, $colour);
+      @if ($brandColour != 'white') {
+        @if ($contrast < $minimum-contrast) {
+          @warn "Contrast ratio for #{$brandColour} too low."
+          + " #{$colour} on #{$govuk-body-background-colour} has a contrast of: #{$contrast}."
+          + " Must be higher than #{$minimum-contrast} for WCAG AA support.";
+        }
+      }
+    }`
+
+    await renderSass({ data: sass, ...sassConfig })
+  })
+})
