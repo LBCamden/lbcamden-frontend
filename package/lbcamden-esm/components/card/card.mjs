77c8d078f3cf1d418cb7f15630f7d67ff6e4dedc
benwatsonuk <hello@benwatson.eu>
2022-06-27 17:09:08 +0100

    Package version bump

---
+++ package/lbcamden-esm/components/card/card.mjs
@@ -0,0 +1,29 @@
+function LBCamdenCard ($module) {
+  this.$module = $module
+}
+
+//
+// /**
+//  * Initialise card
+//  *
+//  * Check for the presence of a card – if any are
+//  * missing then there's nothing to do so return early.
+//  */
+LBCamdenCard.prototype.init = function () {
+  if (!this.$module) {
+    return
+  }
+  if (this.$module.classList.contains('LBCamden-Card--clickable')) {
+    this.handleClickable()
+  }
+}
+
+LBCamdenCard.prototype.handleClickable = function () {
+  if (this.$module.querySelector('a') !== null) {
+    this.$module.addEventListener('click', () => {
+      this.$module.querySelector('a').click()
+    })
+  }
+}
+
+export default LBCamdenCard
---
+++ package/lbcamden/components/card/card.js
@@ -0,0 +1,37 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define('GOVUKFrontend.Card', factory) :
+	(global.GOVUKFrontend = global.GOVUKFrontend || {}, global.GOVUKFrontend.Card = factory());
+}(this, (function () { 'use strict';
+
+function LBCamdenCard ($module) {
+  this.$module = $module;
+}
+
+//
+// /**
+//  * Initialise card
+//  *
+//  * Check for the presence of a card – if any are
+//  * missing then there's nothing to do so return early.
+//  */
+LBCamdenCard.prototype.init = function () {
+  if (!this.$module) {
+    return
+  }
+  if (this.$module.classList.contains('LBCamden-Card--clickable')) {
+    this.handleClickable();
+  }
+};
+
+LBCamdenCard.prototype.handleClickable = function () {
+  if (this.$module.querySelector('a') !== null) {
+    this.$module.addEventListener('click', () => {
+      this.$module.querySelector('a').click();
+    });
+  }
+};
+
+return LBCamdenCard;
+
+})));
