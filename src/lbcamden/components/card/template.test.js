612efee01d283b8066e7f1e6ae8ec8926c14d988
benwatsonuk <hello@benwatson.eu>
2022-08-15 13:28:23 +0100

    Added test coverage to cards

--- src/lbcamden/components/card/template.test.js
+++ src/lbcamden/components/card/template.test.js
@@ -36,19 +36,57 @@ describe('card', () => {
     })
   })
 
-  describe('popular card', () => {
+  describe('card with images', () => {
     it('renders classes', () => {
-      const $ = render('card', examples.popular)
+      const $ = render('card', examples['with image'])
 
       const $component = $('.LBCamden-Card')
-      expect($component.hasClass('LBCamden-Card--popular')).toBeTruthy()
+      expect($component.hasClass('LBCamden-Card--image')).toBeTruthy()
     })
 
-    it('has smaller heading size', () => {
-      const $ = render('card', examples.popular)
+    it('has a content wrapper ', () => {
+      const $ = render('card', examples['with image'])
 
-      const $component = $('.LBCamden-Card h2')
-      expect($component.hasClass('govuk-heading-s')).toBeTruthy()
+      const $component = $('.LBCamden-Card .LBCamden-Card__content_wrapper')
+      expect($component.length).toEqual(1)
+    })
+  })
+
+  describe('card decorations', () => {
+    describe('Type 1', () => {
+      it('renders classes', () => {
+        const $ = render('card', examples['decorated type 1'])
+
+        const $component = $('.LBCamden-Card')
+        expect($component.hasClass('LBCamden-Card--alt-1')).toBeTruthy()
+      })
+    })
+
+    describe('Type 2', () => {
+      it('renders classes', () => {
+        const $ = render('card', examples['decorated type 2'])
+
+        const $component = $('.LBCamden-Card')
+        expect($component.hasClass('LBCamden-Card--alt-2')).toBeTruthy()
+      })
+    })
+
+    describe('Type 3', () => {
+      it('renders classes', () => {
+        const $ = render('card', examples['decorated type 3'])
+
+        const $component = $('.LBCamden-Card')
+        expect($component.hasClass('LBCamden-Card--alt-3')).toBeTruthy()
+      })
+    })
+
+    describe('with border', () => {
+      it('renders classes', () => {
+        const $ = render('card', examples['decorated type 1 with top border'])
+
+        const $component = $('.LBCamden-Card')
+        expect($component.hasClass('LBCamden-Card--top-border')).toBeTruthy()
+      })
     })
   })
 })
