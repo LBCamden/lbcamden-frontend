e435269443ebe4bd74129c0917a45226ebd160a3
Chris Devereux <chrisdevereux@users.noreply.github.com>
2023-05-04 14:23:01 +0100

    add govuk examples

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -1,69 +1,108 @@
 // TODO: we're using a deprecated API as it allows us to programatically create stories
 // this could also be accomplished using the modern API via code-generation using webpack, but this is easier for now
-const {storiesOf} = require('@storybook/html')
-
-// Build up a collection of all our component template files
-const componentsContext = require.context(
-  '../src/lbcamden/components',
-  true,
-  /template\.njk$/,
-)
-
-// Build up a collection of all our yaml files specifying examples
-const yamlContext = require.context(
-  '../src/lbcamden/components',
-  true,
-  /\.yaml$/,
-)
+const { storiesOf } = require("@storybook/html");
 
 // force _all_ nunjucks templates to load so that client-side template imports work
-const allTemplates = require.context(
-  "../src/lbcamden",
-  true,
-  /\.njk$/
-)
-allTemplates.keys().forEach(allTemplates)
+requireAll(require.context("../src/lbcamden", true, /\.njk$/));
 
-// return the component's slug from its path (taken from the directory containing its implementation)
-const getSlug = (x) => {
-  const match = /\/([^\/]*)/.exec(x)
-  return match && match[1]
+// force _all_ nunjucks templates to load so that client-side template imports work
+requireAll(require.context("./govuk", true, /\.njk$/));
+
+buildComponentStories({
+  // Build up a collection of all our component template files
+  componentsContext: require.context(
+    "../src/lbcamden/components",
+    true,
+    /template\.njk$/
+  ),
+
+  // Build up a collection of all our yaml files specifying examples
+  yamlContext: require.context("../src/lbcamden/components", true, /\.yaml$/),
+});
+
+buildComponentStories({
+  // Build up a collection of all our component template files
+  componentsContext: require.context("./govuk/components", true, /template\.njk$/),
+
+  // Build up a collection of all our yaml files specifying examples
+  fixturesContext: require.context("./govuk/components", true, /fixtures\.json$/),
+  optionsContext: require.context("./govuk/components", true, /macro-options\.json$/),
+});
+
+function requireAll(context) {
+  context.keys().forEach((key) => context(key));
+}
+
+function buildComponentStories({
+  componentsContext,
+  yamlContext,
+  fixturesContext,
+  optionsContext,
+}) {
+  const components = buildSlugMap(componentsContext);
+  const fixtures = fixturesContext && buildSlugMap(fixturesContext);
+  const schemas = optionsContext && buildSlugMap(optionsContext);
+  const fixturesWithSchemas = yamlContext && buildSlugMap(yamlContext);
+
+  console.log({
+    components,
+    fixtures,
+    schemas,
+    fixturesWithSchemas
+  })
+
+  // iterate over yaml component schemas and generate stories from examples
+  //
+  // TODO: add a flag to the examples to specify if they should show up here?
+  for (const slug of Object.keys(components)) {
+    console.log(slug)
+
+    if (!components[slug]) {
+      continue;
+    }
+
+    const component = componentsContext(components[slug]).default;
+    let examples, params;
+
+    if (fixturesWithSchemas) {
+      const yaml = yamlContext(fixturesWithSchemas[slug]).default;
+      params = yaml.params;
+      examples = yaml.examples;
+    } else {
+      params = optionsContext(schemas[slug]);
+      examples = fixturesContext(fixtures[slug]).fixtures;
+    }
+
+    const story = storiesOf("Components / " + slug, module);
+    const argTypes = {};
+
+    for (const param of params) {
+      argTypes[param.name] = { type: param.type };
+    }
+
+    for (const example of examples) {
+      if (example.hidden) {
+        continue
+      }
+
+      story.add(example.name, (params) => component({ params }), {
+        component,
+        argTypes,
+        args: example.options ?? example.data,
+      });
+    }
+  }
 }
 
 // map from component slugs to full paths
-const components = Object.fromEntries(
-  componentsContext.keys().map(key => [getSlug(key), key])
-)
+function buildSlugMap(context) {
+  return Object.fromEntries(
+    context.keys().map((key) => [getSlug(key), key])
+  );
+}
 
-// iterate over yaml component schemas and generate stories from examples
-//
-// TODO: add a flag to the examples to specify if they should show up here?
-for (const key of yamlContext.keys()) {
-  const slug = getSlug(key)
-
-  if (!components[slug]) {
-    continue
-  }
-
-  const component = componentsContext(components[slug]).default
-  const yaml = yamlContext(key).default
-
-  const story = storiesOf('Components / ' + slug, module)
-  const argTypes = {}
-
-  for (const param of yaml.params) {
-    argTypes[param.name] = { type: param.type }
-  }
-
-  for (const example of yaml.examples) {
-    story.add(
-      example.name,
-      (params) => component({ params }),
-      {
-        component,
-        argTypes,
-        args: example.data
-      }
-    )
-  }
+// return the component's slug from its path (taken from the directory containing its implementation)
+function getSlug(x) {
+  const match = /\/([^\/]*)/.exec(x);
+  return match && match[1];
 }
