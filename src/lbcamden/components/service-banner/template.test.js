b8db247af64a8047b30bb57de2ec2a895b9cc9fc
Chris Devereux <chrisdevereux@users.noreply.github.com>
2023-06-07 10:25:19 +0100

    rename status -> service-banner

--- src/lbcamden/components/service-banner/template.test.js
+++ src/lbcamden/components/service-banner/template.test.js
@@ -7,30 +7,30 @@ const axe = require('../../../../lib/axe-helper')
 
 const { render, getExamples } = require('../../../../lib/jest-helpers')
 
-const examples = getExamples('status')
-describe('status', () => {
+const examples = getExamples('service-banner')
+describe('service-banner', () => {
   describe('default example', () => {
     it('passes accessibility tests', async () => {
-      const $ = render('status', examples.default)
+      const $ = render('service-banner', examples.default)
 
       const results = await axe($.html())
       expect(results).toHaveNoViolations()
     })
 
     it('has a role of `banner`', () => {
-      const $ = render('status', examples.default)
+      const $ = render('service-banner', examples.default)
 
-      const $component = $('.lbcamden-status')
+      const $component = $('.lbcamden-service-banner')
       expect($component.attr('role')).toEqual('banner')
     })
   })
 
   describe('custom options', () => {
     it('renders classes', () => {
-      const $ = render('status', examples.classes)
+      const $ = render('service-banner', examples.classes)
 
-      const $component = $('.lbcamden-status')
-      expect($component.hasClass('app-status--custom-modifier')).toBeTruthy()
+      const $component = $('.lbcamden-service-banner')
+      expect($component.hasClass('app-service-banner--custom-modifier')).toBeTruthy()
     })
   })
 })
