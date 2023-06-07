34b182133c1c71c7cc22e9a1f26ef92ee897e729
Chris Devereux <chrisdevereux@users.noreply.github.com>
2023-06-07 10:30:57 +0100

    emergency -> emergency-banner

--- src/lbcamden/components/emergency-banner/template.test.js
+++ src/lbcamden/components/emergency-banner/template.test.js
@@ -7,30 +7,30 @@ const axe = require('../../../../lib/axe-helper')
 
 const { render, getExamples } = require('../../../../lib/jest-helpers')
 
-const examples = getExamples('emergency')
-describe('emergency', () => {
+const examples = getExamples('emergency-banner')
+describe('emergency-banner', () => {
   describe('default example', () => {
     it('passes accessibility tests', async () => {
-      const $ = render('emergency', examples.default)
+      const $ = render('emergency-banner', examples.default)
 
       const results = await axe($.html())
       expect(results).toHaveNoViolations()
     })
 
     it('has a role of `banner`', () => {
-      const $ = render('emergency', examples.default)
+      const $ = render('emergency-banner', examples.default)
 
-      const $component = $('.lbcamden-emergency')
+      const $component = $('.lbcamden-emergency-banner')
       expect($component.attr('role')).toEqual('banner')
     })
   })
 
   describe('custom options', () => {
     it('renders classes', () => {
-      const $ = render('emergency', examples.classes)
+      const $ = render('emergency-banner', examples.classes)
 
-      const $component = $('.lbcamden-emergency')
-      expect($component.hasClass('app-emergency--custom-modifier')).toBeTruthy()
+      const $component = $('.lbcamden-emergency-banner')
+      expect($component.hasClass('app-emergency-banner--custom-modifier')).toBeTruthy()
     })
   })
 })
