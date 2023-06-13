e57d0020cd266145a9c732d93dc57a014d577996
Alex Wybraniec <alex.wybraniec@camden.gov.uk>
2023-06-13 11:07:34 +0100

    related content card

---
+++ src/lbcamden/components/related-content-card/template.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('related-content-card')
+describe('related-content-card', () => {
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('related-content-card', examples.default)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
