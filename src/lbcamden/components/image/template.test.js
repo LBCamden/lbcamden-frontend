7c831aba77b27d931ee78c6542f3666a43cd7bfa
Chris Devereux <chrisdevereux@users.noreply.github.com>
2023-06-19 18:59:13 +0100

    fix handling of media queries

--- src/lbcamden/components/image/template.test.js
+++ src/lbcamden/components/image/template.test.js
@@ -23,16 +23,26 @@ describe('image', () => {
       const $source = $('.lbcamden-image source[type="image/webp"]')
 
       expect($source.attr('srcset')).toEqual('/example-assets/images/300x200.webp 300w, /example-assets/images/600x400.webp 600w, /example-assets/images/1200x800.webp 1200w, /example-assets/images/1800x1200.webp 1800w')
-      expect($source.attr('sizes')).toEqual('min-width(320px) 100vw, min-width(768px) 50vw, min-width(992px) 25vw')
+      expect($source.attr('sizes')).toEqual('100vw')
     })
 
     it('renders attributes on the img element', async () => {
       const $ = render('image', examples.default)
       const $img = $('.lbcamden-image img')
 
-      expect($img.attr('src')).toEqual(examples.default.fallback)
+      expect($img.attr('src')).toEqual(examples.default.src)
       expect($img.attr('alt')).toEqual(examples.default.alt)
       expect($img.attr('loading')).toEqual('lazy')
+    })
+  })
+
+  describe('multiple viewports example', () => {
+    it('renders srcset and sizes attributes on the source element', async () => {
+      const $ = render('image', examples['with multiple viewports'])
+      const $source = $('.lbcamden-image source[type="image/webp"]')
+
+      expect($source.attr('srcset')).toEqual('/example-assets/images/300x200.webp 300w, /example-assets/images/600x400.webp 600w, /example-assets/images/1200x800.webp 1200w, /example-assets/images/1800x1200.webp 1800w')
+      expect($source.attr('sizes')).toEqual('(min-width: 768px) 50vw, 100vw')
     })
   })
 
@@ -48,7 +58,7 @@ describe('image', () => {
       const $ = render('image', examples['single source'])
       const $img = $('.lbcamden-image')
 
-      expect($img.attr('src')).toEqual(examples['single source'].fallback)
+      expect($img.attr('src')).toEqual(examples['single source'].src)
       expect($img.attr('alt')).toEqual(examples['single source'].alt)
       expect($img.attr('width')).toEqual('300')
       expect($img.attr('height')).toEqual('200')
@@ -62,7 +72,7 @@ describe('image', () => {
         it('renders classes correctly', () => {
           const $ = render('image', examples[`classes ${variant}`])
 
-          const $component = $('img')
+          const $component = $('.lbcamden-image')
           expect($component.hasClass('app-image--custom-modifier')).toBeTruthy()
         })
 
