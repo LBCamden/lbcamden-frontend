151be1e8342a22234893b87501b21eee0b7e8819
Chris Devereux <chrisdevereux@users.noreply.github.com>
2023-06-20 10:08:12 +0100

    remove picture element box from page layout

--- src/lbcamden/components/image/template.test.js
+++ src/lbcamden/components/image/template.test.js
@@ -20,7 +20,7 @@ describe('image', () => {
 
     it('renders srcset and sizes attributes on the source element', async () => {
       const $ = render('image', examples.default)
-      const $source = $('.lbcamden-image source[type="image/webp"]')
+      const $source = $('source[type="image/webp"]')
 
       expect($source.attr('srcset')).toEqual('/example-assets/images/300x200.webp 300w, /example-assets/images/600x400.webp 600w, /example-assets/images/1200x800.webp 1200w, /example-assets/images/1800x1200.webp 1800w')
       expect($source.attr('sizes')).toEqual('100vw')
@@ -28,7 +28,7 @@ describe('image', () => {
 
     it('renders attributes on the img element', async () => {
       const $ = render('image', examples.default)
-      const $img = $('.lbcamden-image img')
+      const $img = $('.lbcamden-image')
 
       expect($img.attr('src')).toEqual(examples.default.src)
       expect($img.attr('alt')).toEqual(examples.default.alt)
@@ -39,7 +39,7 @@ describe('image', () => {
   describe('multiple viewports example', () => {
     it('renders srcset and sizes attributes on the source element', async () => {
       const $ = render('image', examples['with multiple viewports'])
-      const $source = $('.lbcamden-image source[type="image/webp"]')
+      const $source = $('source[type="image/webp"]')
 
       expect($source.attr('srcset')).toEqual('/example-assets/images/300x200.webp 300w, /example-assets/images/600x400.webp 600w, /example-assets/images/1200x800.webp 1200w, /example-assets/images/1800x1200.webp 1800w')
       expect($source.attr('sizes')).toEqual('(min-width: 768px) 50vw, 100vw')
@@ -67,22 +67,18 @@ describe('image', () => {
   })
 
   describe('custom options', () => {
-    ['single source', 'multiple source'].forEach(variant => {
-      describe(variant, () => {
-        it('renders classes correctly', () => {
-          const $ = render('image', examples[`classes ${variant}`])
+    it('renders classes correctly', () => {
+      const $ = render('image', examples.classes)
 
-          const $component = $('.lbcamden-image')
-          expect($component.hasClass('app-image--custom-modifier')).toBeTruthy()
-        })
+      const $component = $('.lbcamden-image')
+      expect($component.hasClass('app-image--custom-modifier')).toBeTruthy()
+    })
 
-        it('renders custom attributes on the img element', () => {
-          const $ = render('image', examples[`attributes ${variant}`])
+    it('renders custom attributes on the img element', () => {
+      const $ = render('image', examples.attributes)
 
-          const $component = $('img')
-          expect($component.attr('data-test-attribute')).toEqual('value')
-        })
-      })
+      const $component = $('img')
+      expect($component.attr('data-test-attribute')).toEqual('value')
     })
   })
 })
