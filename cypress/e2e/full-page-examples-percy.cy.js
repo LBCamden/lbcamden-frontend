fe68f5f2be1ef61f715ee3ff612806ad3924c296
Alex Wybraniec <alex.wybraniec@camden.gov.uk>
2023-08-11 10:17:27 +0100

    simple cypress test to run percy on some pages

---
+++ cypress/e2e/full-page-examples-percy.cy.js
@@ -0,0 +1,36 @@
+// import * as urls from '../../_site/urls.js'
+
+const urls = [
+  '/full-page-examples/all-services', 
+  '/full-page-examples/article',
+  '/full-page-examples/category-page',
+  '/full-page-examples/category-page-housing',
+  '/full-page-examples/check-your-answers',
+  '/full-page-examples/cookie-consent-page',
+  '/full-page-examples/home-page',
+  '/full-page-examples/home-page-engagement',
+  '/full-page-examples/search'
+];
+// const excludePagesBreadcrumb = ['/404.html', '/', '/typography/', '/typography-alt/', '/typography-alt-1/', '/accessibility/'];
+// const excludePagesCGULinkInPhaseBanner = ['/404.html', '/', '/typography/', '/typography-alt/', '/typography-alt-1/', '/accessibility/', '/cookies/'];
+const viewports = ['macbook-13', 'iphone-5']
+
+describe('Full Page examples visual regression test', () => {
+
+  urls.forEach(url => {
+    viewports.forEach(viewport => {
+  
+        describe(`url: ${url} (${viewport})`, () => {
+    
+            it(`Should display breadcrumb, should have well formed internal urls ${url}`, () => {
+              //uses the URL alias above
+              cy.viewport(viewport);
+              cy.visit('http://localhost:3000' + url);
+              
+              cy.percySnapshot();
+
+            });
+        });
+    })
+  });
+});
