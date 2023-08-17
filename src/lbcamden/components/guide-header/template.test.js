7145385a1aaabe1b0b8b7709a7516a239063c885
Chris Devereux <361391+chrisdevereux@users.noreply.github.com>
2023-08-17 16:19:47 +0100

    Guide page components & full-page example (#128)
    * first pass guide page
    
    * clean up tests
    
    * fix chrome issue; remove link underlines
    
    * handling of missing hrefs
    
    * pull related items up on example page
    
    * rename files
    
    * scroll to hash
    
    * fix misnamed class
    
    * not found page
    
    * update page example to be clearer about intent of hash links
    
    * add missing hash
    
    * add fixtures for error messages
    
    * remove scroll-to-anchor behaviour
    
    * move error messages out of markup
    
    ---------
    
    Co-authored-by: Chris Devereux <chrisdevereux@users.noreply.github.com>

---
+++ src/lbcamden/components/guide-header/template.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('guide-header')
+
+describe('guide-header', () => {
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('guide-header', examples.default)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
