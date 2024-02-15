b386ed0b994b3d23be55e6aed20e01334459e16a
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2024-02-15 10:07:31 +0000

    add config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+
+  return fs.readJson(pathToConfigFile)
+}
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const file = config.env.configFile || 'development'
+
+      return getConfigurationByFile(file)
     },
   },
-  video: false,
-  screenshotOnRunFailure: false
 });
