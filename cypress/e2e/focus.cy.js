ee44138e81de25f04218b75dc6764b8fc02f1a10
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2024-02-20 13:07:49 +0000

    should work; tests are all pending?

--- cypress/e2e/focus.cy.js
+++ cypress/e2e/focus.cy.js
@@ -1,179 +1,59 @@
-describe('Header focus test', () => {
-  it('should display focus states for each top level link on desktop', () => {
-    cy.viewport("macbook-13");
-    cy.visit('http://localhost:3000/components/header/with-mixed-levels-of-navigation/preview');
-    cy.window().focus();
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-  })
+const waitTime = 500;
+const viewports = { "desktop": 'macbook-13', "mobile": 'iphone-6+' };
+const domain = 'http://localhost:3000'
+const focusTests = {
+  "header": {
+    "url": '/components/header/with-mixed-levels-of-navigation/preview',
+    "tests": {
+      "top level links": {
+        "desktop": ['tab', 'tab', 'tab', 'tab', 'tab'],
+        "mobile": ['tab', 'tab', 'enter', 'tab', 'tab', 'tab', 'tab', 'enter', 'tab', 'tab']
+      },
+      "second level links": {
+        "desktop": ['tab', 'tab', 'tab', 'enter', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'enter', 'tab', 'tab'],
+        "mobile": ['tab', 'tab', 'enter', 'tab', 'tab', 'tab', 'tab', 'enter', 'tab', 'tab']
+      }
+    }
+  },
+  "footer": {
+    "url": '/components/footer/with-primary,-secondary-and-utility-navigation,-sponsor-image-and-social-navigation/preview',
+    "tests": {
+      "links": {
+        "desktop": ['tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab'],
+        "mobile": ['tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab']
+      }
+    }
+  }
+};
 
-  it('should display focus states for each top level link on mobile', () => {
-    cy.viewport("iphone-6+");
-    cy.visit('http://localhost:3000/components/header/with-mixed-levels-of-navigation/preview');
-    cy.window().focus();
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-  })
-
-  it('should display focus states for second level links and components on desktop', () => {
-    cy.viewport("macbook-13");
-    cy.visit('http://localhost:3000/components/header/with-mixed-levels-of-navigation/preview');
-    cy.window().focus();
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.focused().realPress('Enter');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab')
-    cy.wait(500)
-    cy.realPress('Enter');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab')
-    cy.wait(500)
-  })
-
-  it('should display focus states for second level links and components on mobile', () => {
-    cy.viewport("iphone-6+");
-    cy.visit('http://localhost:3000/components/header/with-mixed-levels-of-navigation/preview');
-    cy.window().focus();
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.focused().realPress('Enter');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-    cy.focused().realPress('Enter');
-    cy.wait(500)
-    cy.realPress('Tab')
-    cy.wait(500)
-    cy.realPress('Tab');
-    cy.wait(500)
-  })
-})
-
-describe('Footer focus test', () => {
-  it('should show focus states on all footer link elements in desktop', () => {
-    cy.viewport("macbook-13");
-    cy.visit('http://localhost:3000/components/footer/with-primary,-secondary-and-utility-navigation,-sponsor-image-and-social-navigation/preview')
-    cy.window().focus();
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-  })
-
-  it('should show focus states on all footer link elements in mobile', () => {
-    cy.viewport("iphone-6+");
-    cy.visit('http://localhost:3000/components/footer/with-primary,-secondary-and-utility-navigation,-sponsor-image-and-social-navigation/preview')
-    cy.window().focus();
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
-    cy.realPress('Tab');
-    cy.wait(500);
+describe('Focus tests', () => {
+  Object.keys(focusTests).forEach(test => {
+    Object.keys(focusTests[test].tests).forEach(currentTest => {
+      Object.keys(focusTests[test].tests[currentTest]).forEach(viewport => {
+        it(`should display focus states for ${test} ${currentTest} for ${viewport}`), () => {
+          cy.viewport(viewports[viewport])
+          cy.visit(domain + focusTests[test].url)
+          cy.window().focus();
+          cy.wait(500)
+          focusTests[test].tests[currentTest][viewport].forEach(testAction => {
+            switch (testAction) {
+              case 'tab':
+                cy.realPress('Tab');
+                cy.wait(500)
+                break;
+              case 'enter':
+                cy.realPress('Enter');
+                cy.wait(500)
+                break;
+              default:
+                //throw exception
+                console.log('unknown action')
+                break;
+            }
+          }
+          )
+        }
+      })
+    })
   })
 })
