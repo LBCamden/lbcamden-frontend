8eeb127400394466ea0e65da610087bc9a2f7068
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2024-10-15 14:37:11 +0100

    test updates

--- src/lbcamden/components/campaign-hero/template.test.js
+++ src/lbcamden/components/campaign-hero/template.test.js
@@ -7,21 +7,21 @@ const axe = require('../../../../lib/axe-helper')
 
 const { render, getExamples } = require('../../../../lib/jest-helpers')
 
-const examples = getExamples('info-callout')
+const examples = getExamples('campaign-hero')
 
-describe('info-callout', () => {
+describe('campaign-hero', () => {
     describe('default example', () => {
         it('passes accessibility tests', async () => {
-            const $ = render('info-callout', examples.default)
+            const $ = render('card-gallery', examples.default)
 
             const results = await axe($.html())
             expect(results).toHaveNoViolations()
         })
 
         it('renders contents', () => {
-            const $ = render('info-callout', examples.default)
+            const $ = render('campaign-hero', examples.default)
 
-            expect($('h2').text()).toContain('Heading')
+            expect($('h2').text()).toContain('Welcome to Camden.gov.uk')
             //   expect($('.lbcamden-card-gallery__description').text()).toContain('Find relevant services')
 
             //   expect($('li a').first().text()).toContain('Starting school')
---
+++ src/lbcamden/components/image-gallery/template.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('image-gallery')
+
+describe('image-gallery', () => {
+    describe('default example', () => {
+        it('passes accessibility tests', async () => {
+            const $ = render('image-gallery', examples.default)
+
+            const results = await axe($.html())
+            expect(results).toHaveNoViolations()
+        })
+
+        it('renders contents', () => {
+            const $ = render('image-gallery', examples.default)
+
+            expect($('h2').text()).toContain('Get more from Camden')
+            expect($('.lbcamden-card-gallery__description').text()).toContain('We provide services and information for residents, businesses and visitors to the London Borough of Camden.')
+        })
+    })
+})
