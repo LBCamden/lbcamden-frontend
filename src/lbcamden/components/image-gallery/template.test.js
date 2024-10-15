f604650f15e7cb2dced65cf5e67fac4d33245a2e
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2024-10-15 15:39:33 +0100

    test updates: spacing

--- src/lbcamden/components/image-gallery/template.test.js
+++ src/lbcamden/components/image-gallery/template.test.js
@@ -10,19 +10,19 @@ const { render, getExamples } = require('../../../../lib/jest-helpers')
 const examples = getExamples('image-gallery')
 
 describe('image-gallery', () => {
-    describe('default example', () => {
-        it('passes accessibility tests', async () => {
-            const $ = render('image-gallery', examples.default)
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('image-gallery', examples.default)
 
-            const results = await axe($.html())
-            expect(results).toHaveNoViolations()
-        })
-
-        it('renders contents', () => {
-            const $ = render('image-gallery', examples.default)
-
-            expect($('h2').text()).toContain('Get more from Camden')
-            expect($('.lbcamden-card-gallery__description').text()).toContain('We provide services and information for residents, businesses and visitors to the London Borough of Camden.')
-        })
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
     })
+
+    it('renders contents', () => {
+      const $ = render('image-gallery', examples.default)
+
+      expect($('h2').text()).toContain('Get more from Camden')
+      expect($('.lbcamden-card-gallery__description').text()).toContain('We provide services and information for residents, businesses and visitors to the London Borough of Camden.')
+    })
+  })
 })
