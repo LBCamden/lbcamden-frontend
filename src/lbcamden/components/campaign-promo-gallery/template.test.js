2ad104aed24b35fb4af6d2cc3c755a7f939354ec
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2024-10-24 11:22:21 +0100

    change tests

--- src/lbcamden/components/campaign-promo-gallery/template.test.js
+++ src/lbcamden/components/campaign-promo-gallery/template.test.js
@@ -7,23 +7,23 @@ const axe = require('../../../../lib/axe-helper')
 
 const { render, getExamples } = require('../../../../lib/jest-helpers')
 
-const examples = getExamples('campaign-engagement-banner')
+const examples = getExamples('campaign-promo-gallery')
 
-describe('campaign-engagement-banner', () => {
+describe('campaign-promo-banner', () => {
   describe('default example', () => {
     it('passes accessibility tests', async () => {
-      const $ = render('card-gallery', examples.default)
+      const $ = render('campaign-promo-gallery', examples.default)
 
       const results = await axe($.html())
       expect(results).toHaveNoViolations()
     })
 
     it('renders contents', () => {
-      const $ = render('campaign-engagement-banner', examples.default)
+      const $ = render('campaign-promo-gallery', examples.default)
 
       expect($('h2').text()).toContain('Heading')
-      expect($('p').text()).toContain('Short description')
-      expect($('a').attr('href')).toContain('/test')
+      expect($('a').first().attr('href')).toContain('#1')
+      expect($('a').first().text()).toContain('Housing Repairs')
     })
   })
 })
