ea2088577f9569431a631ddd10f2b85fdc0de3a5
Alex Wybraniec <alex.wybraniec@camden.gov.uk>
2024-12-18 09:49:59 +0000

    set filename of CSS output, but need to stop it inlining all fonts

--- vite.config.dist.mjs
+++ vite.config.dist.mjs
@@ -24,48 +24,81 @@ export default defineConfig({
     assetsDir: 'assets',
     emptyOutDir: true,
     sourcemap: true,
-    rollupOptions: {
-      
-      treeshake: false,
-      
-      input: {
-        govuk_js: './node_modules/govuk-frontend/govuk/all.js',
-        lbcamden_js: './src/lbcamden/all.js',
-        lbcamden_css: './src/lbcamden/all.scss',
+    // assetsInlineLimit: 0,
+    lib:{
+      entry: {
+        govuk_js: '../../node_modules/govuk-frontend/govuk/all.js',
+        lbcamden_js: './all.js',
+        lbcamden_css: './all.scss',
       },
-      output: {
-        manualChunks: false,
-        // generatedCode: 'es2015',
-        format: 'es',
-        entryFileNames: function (file) {
+      formats: ['es'],
+      fileName: (format, entryName) => {
 
-          if (file.name === 'lbcamden_js') {
+          
+
+          if (entryName === 'lbcamden_js') {
             return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.js'
           }
 
-          if (file.name === 'govuk_js') {
+          if (entryName === 'govuk_js') {
             return 'govuk-frontend-4.7.0.min.js' // AW: MUST FIX: MAGIC STRING
           }
 
-          return 'unknown-entry/[name]'
-        },
-        assetFileNames: function (file) {
+          // if (entryName === 'lbcamden_css') {
+          //   console.log("AAA" + entryName)
+          //   return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.css'
+          // }
 
-          // Rename CSS output
-          if (file.name === 'lbcamden_css.css') {
-            return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.css'
-          }
+        return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.js'
+      },
+      cssFileName: 'lbcamden-frontend-' + (process.env.npm_package_version),
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.[ext]'
+      },
+    },
+    // rollupOptions: {
+      
+    //   treeshake: false,
+    //   input: {
+    //     // govuk_js: './node_modules/govuk-frontend/govuk/all.js',
+    //     // lbcamden_js: './src/lbcamden/all.js',
+    //     lbcamden_css: './src/lbcamden/all.scss',
+    //   },
+    //   output: {
+    //     manualChunks: false,
+    //     // generatedCode: 'es2015',
+    //     // format: 'es',
+    //     entryFileNames: function (file) {
 
-          // Handle fonts parsed by scss
-          if (file.name.endsWith('woff') || file.name.endsWith('woff2')) {
-            return 'assets/fonts/[name].[ext]'
-          }
+    //       if (file.name === 'lbcamden_js') {
+    //         return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.js'
+    //       }
 
-          return 'assets/[name].[ext]'
-        }
+    //       if (file.name === 'govuk_js') {
+    //         return 'govuk-frontend-4.7.0.min.js' // AW: MUST FIX: MAGIC STRING
+    //       }
 
-      }
-    }
+    //       return 'unknown-entry/[name]'
+    //     },
+    //     assetFileNames: function (file) {
+
+    //       // Rename CSS output
+    //       if (file.name === 'lbcamden_css.css') {
+    //         return 'lbcamden-frontend-' + (process.env.npm_package_version) + '.min.css'
+    //       }
+
+    //       // Handle fonts parsed by scss
+    //       if (file.name.endsWith('woff') || file.name.endsWith('woff2')) {
+    //         return 'assets/fonts/[name].[ext]'
+    //       }
+
+    //       return 'assets/[name].[ext]'
+    //     }
+
+    //   }
+    // }
   }
 
 })
