a4f93e5d83078c520b05a6ecfcaa02930fb73e68
Alex Wybraniec <alex.wybraniec@camden.gov.uk>
2025-01-15 10:39:46 +0000

    Add package and dist dirs back into repos. We may decide to remove these again in the future

---
+++ package/lbcamden/all.js
@@ -0,0 +1,43 @@
+import { nodeListForEach } from './common'
+import LBCamdenButton from './components/button/button'
+import LBCamdenGuideContent from './components/guide-content/guide-content'
+import LBCamdenGuideHeader from './components/guide-header/guide-header'
+import LBCamdenHeader from './components/header/header'
+
+import './all.scss'
+
+function initAll (options) {
+  options = typeof options !== 'undefined' ? options : {}
+
+  // Allow the user to initialise GOV.UK Frontend in only certain sections of the page
+  // Defaults to the entire document if nothing is set.
+  var scope = typeof options.scope !== 'undefined' ? options.scope : document
+
+  var $LBCamdenHeaders = scope.querySelectorAll('[data-module="lbcamden-header"]')
+  nodeListForEach($LBCamdenHeaders, function ($LBCamdenHeader) {
+    new LBCamdenHeader($LBCamdenHeader).init()
+  })
+
+  var $LBCamdenButtons = scope.querySelectorAll('[data-module="lbcamden-button"]')
+  nodeListForEach($LBCamdenButtons, function ($LBCamdenButton) {
+    new LBCamdenButton($LBCamdenButton).init()
+  })
+
+  var $LBCamdenGuideContents = scope.querySelectorAll('[data-module="lbcamden-guide-content"]')
+  nodeListForEach($LBCamdenGuideContents, function ($LBCamdenGuideContent) {
+    new LBCamdenGuideContent($LBCamdenGuideContent).init()
+  })
+
+  var $LBCamdenGuideHeaders = scope.querySelectorAll('[data-module="lbcamden-guide-header"]')
+  nodeListForEach($LBCamdenGuideHeaders, function ($LBCamdenGuideHeader) {
+    new LBCamdenGuideHeader($LBCamdenGuideHeader).init()
+  })
+}
+
+export {
+  LBCamdenButton,
+  LBCamdenGuideContent,
+  LBCamdenGuideHeader,
+  LBCamdenHeader,
+  initAll
+}
