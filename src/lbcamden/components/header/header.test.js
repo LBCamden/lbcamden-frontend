12f144d2bfff07e5702bc262e1559f7dbdaf4c92
Chris Devereux <361391+chrisdevereux@users.noreply.github.com>
2025-08-13 12:58:42 +0100

    DS-570: Screen reader issues in header component (#248)
    * reformat header.js
    
    * fix incorrect aria-label when closing search
    
    * respect data-text-for-show/hide
    
    * add tests for header js changes
    
    * target -> currentTarget
    
    ---------
    
    Co-authored-by: Chris Devereux <chrisdevereux@users.noreply.github.com>

--- src/lbcamden/components/header/header.test.js
+++ src/lbcamden/components/header/header.test.js
@@ -1,64 +1,128 @@
-import { describe, expect, it, beforeEach } from 'vitest'
+import { describe, expect, it, beforeEach } from "vitest";
 import { configureAxe } from "vitest-axe";
 const axe = configureAxe({
   rules: {
-    'skip-link': { enabled: false },
-    region: { enabled: false }
-  }
-})
+    "skip-link": { enabled: false },
+    region: { enabled: false },
+  },
+});
 
-const { getExamples, renderDOM, setViewportWidth } = require('../../../../lib/jest-helpers')
+const {
+  getExamples,
+  renderDOM,
+  setViewportWidth,
+} = require("../../../../lib/jest-helpers");
 // const LBCamdenHeader = require('./header').default
 // const LBCamdenButton = require('../button/button').default
-import LBCamdenHeader from './header'
-import LBCamdenButton from '../button/button'
+import LBCamdenHeader from "./header";
+import LBCamdenButton from "../button/button";
 
-const userEvent = require('@testing-library/user-event').default
+const userEvent = require("@testing-library/user-event").default;
 
-const examples = getExamples('header')
+const examples = getExamples("header");
 
 beforeEach(() => {
-  window.sessionStorage.clear()
-  window.location.hash = ''
+  window.sessionStorage.clear();
+  window.location.hash = "";
 
   for (const child of document.body.children) {
-    child.remove()
+    child.remove();
   }
-})
+});
 
-describe('/components/header', () => {
-  describe('/components/header/preview', () => {
-    describe('when JavaScript is available', () => {
-      const setup = (example = examples.default) => renderDOM('header', example, {
-        jsModules: [LBCamdenHeader, LBCamdenButton]
-      })
+describe("/components/header", () => {
+  describe("/components/header/preview", () => {
+    describe("when JavaScript is available", () => {
+      const setup = (example = examples.default) =>
+        renderDOM("header", example, {
+          jsModules: [LBCamdenHeader, LBCamdenButton],
+        });
 
-      describe('on desktop devices', () => {
-        beforeEach(() => setViewportWidth(1024))
+      describe("on desktop devices", () => {
+        beforeEach(() => setViewportWidth(1024));
 
-        describe('the navigation function', () => {
-          it('the menu toggle button should be hidden', async () => {
-            const target = setup().querySelector('button[aria-controls="super-navigation-menu"]')
-            expect(target.hidden).toEqual(true)
-          })
-        })
-      })
+        describe("opening a nav item with sub-items", () => {
+          it("should change the aria-label text from show to hide and the aria-expanded to true", async () => {
+            const user = userEvent.setup();
+            const example = setup(
+              examples["Nested navigation (with three columns)"],
+            );
+            const item = example.querySelector(
+              ".lbcamden-header__navigation-second-toggle-button",
+            );
 
-      describe('on all devices', () => {
-        describe('the search function', () => {
-          it('clicking search button should give self \'open\' class', async () => {
-            const target = setup()
-            const user = userEvent.setup()
+            await user.click(item);
 
-            const searchButton = target.querySelector('button[aria-controls="super-search-menu"]')
-            const initialState = searchButton.classList.contains('lbcamden-header__open-button')
+            expect(item.ariaLabel).toEqual("Hide Navigation item 1 menu");
+            expect(item.ariaExpanded).toEqual("true");
+          });
 
-            await user.click(searchButton)
-            const newState = searchButton.classList.contains('lbcamden-header__open-button')
+          it("should change the aria-label text for previously expanded items from hide to show and the aria-expanded to false", async () => {
+            const user = userEvent.setup();
+            const example = setup(
+              examples["Nested navigation (with three columns)"],
+            );
+            const [oldItem, newItem] = example.querySelectorAll(
+              ".lbcamden-header__navigation-second-toggle-button",
+            );
 
-            expect(initialState).toBe(false)
-            expect(newState).toBe(true)
-          }, 60000)
+            await user.click(oldItem);
+            await user.click(newItem);
+
+            expect(oldItem.ariaLabel).toEqual("Show Navigation item 1 menu");
+            expect(oldItem.ariaExpanded).toEqual("false");
+          });
+        });
+
+        describe("closing a nav item with sub-items", () => {
+          it("should change the aria-label text from hide to show and the aria-expanded to false", async () => {
+            const user = userEvent.setup();
+            const example = setup(
+              examples["Nested navigation (with three columns)"],
+            );
+            const item = example.querySelector(
+              ".lbcamden-header__navigation-second-toggle-button",
+            );
+
+            await user.click(item);
+            await user.click(item);
+
+            expect(item.ariaLabel).toEqual("Show Navigation item 1 menu");
+            expect(item.ariaExpanded).toEqual("false");
+          });
+        });
+
+        describe("the navigation function", () => {
+          it("the menu toggle button should be hidden", async () => {
+            const target = setup().querySelector(
+              'button[aria-controls="super-navigation-menu"]',
+            );
+            expect(target.hidden).toEqual(true);
+          });
+        });
+      });
+
+      describe("on all devices", () => {
+        describe("the search function", () => {
+          it("clicking search button should give self 'open' class and update aria attributes", async () => {
+            const target = setup();
+            const user = userEvent.setup();
+
+            const searchButton = target.querySelector(
+              'button[aria-controls="super-search-menu"]',
+            );
+            const initialState = searchButton.classList.contains(
+              "lbcamden-header__open-button",
+            );
+
+            await user.click(searchButton);
+            const newState = searchButton.classList.contains(
+              "lbcamden-header__open-button",
+            );
+
+            expect(initialState).toBe(false);
+            expect(newState).toBe(true);
+          }, 60000);
           //
           // it('clicking search button should move focus to search input', async () => {
           //   await page.emulate(iPhone)
@@ -71,119 +135,138 @@ describe('/components/header', () => {
           //   expect(newState._remoteObject.className).toBe('HTMLInputElement')
           // })
 
-          it('the search container should be hidden', async () => {
-            const target = setup().querySelector('#super-search-menu')
-            expect(target.hasAttribute('hidden')).toEqual(true)
-          })
-        })
+          it("the search container should be hidden", async () => {
+            const target = setup().querySelector("#super-search-menu");
+            expect(target.hasAttribute("hidden")).toEqual(true);
+          });
+        });
         //
         // describe('the navigation function', () => {
         //
         // })
-      })
+      });
 
-      describe('on mobile devices', () => {
-        beforeEach(() => setViewportWidth(320))
+      describe("on mobile devices", () => {
+        beforeEach(() => setViewportWidth(320));
 
         // describe('the search function', () => {
         //
         // })
-        describe('the mobile nav function', () => {
-          it('the menu container should be hidden', async () => {
-            const target = setup().querySelector('#super-search-menu')
-            expect(target.hasAttribute('hidden')).toEqual(true)
-          })
+        describe("the mobile nav function", () => {
+          it("the menu container should be hidden", async () => {
+            const target = setup().querySelector("#super-search-menu");
+            expect(target.hasAttribute("hidden")).toEqual(true);
+          });
 
-          it('the menu toggle button should not be hidden', async () => {
-            const target = setup().querySelector('button[aria-controls="super-navigation-menu"]')
-            expect(target.hasAttribute('hidden')).toEqual(false)
-          })
+          it("the menu toggle button should not be hidden", async () => {
+            const target = setup().querySelector(
+              'button[aria-controls="super-navigation-menu"]',
+            );
+            expect(target.hasAttribute("hidden")).toEqual(false);
+          });
 
-          it('the menu toggle button should have aria-expanded value of false', async () => {
-            const target = setup().querySelector('button[aria-controls="super-navigation-menu"]')
-            expect(target.getAttribute('aria-expanded')).toBe('false')
-          })
+          it("the menu toggle button should have aria-expanded value of false", async () => {
+            const target = setup().querySelector(
+              'button[aria-controls="super-navigation-menu"]',
+            );
+            expect(target.getAttribute("aria-expanded")).toBe("false");
+          });
 
-          it('the menu toggle button should expose navigation options when clicked', async () => {
-            const user = userEvent.setup()
-            const target = setup()
-            const menu = target.querySelector('#super-navigation-menu')
-            const initialState = menu.hidden
+          it("the menu toggle button should expose navigation options when clicked", async () => {
+            const user = userEvent.setup();
+            const target = setup();
+            const menu = target.querySelector("#super-navigation-menu");
+            const initialState = menu.hidden;
 
-            await user.click(target.querySelector('button[aria-controls="super-navigation-menu"]'))
-            const newState = menu.hidden
+            await user.click(
+              target.querySelector(
+                'button[aria-controls="super-navigation-menu"]',
+              ),
+            );
+            const newState = menu.hidden;
 
-            expect(initialState).toEqual(true)
-            expect(newState).toEqual(false)
-          })
-        })
-      })
-    })
-    describe('when JavaScript is not available', () => {
-      const setup = (example = examples.default) => renderDOM('header', example, {
-        jsEnabled: false
-      })
+            expect(initialState).toEqual(true);
+            expect(newState).toEqual(false);
+          });
+        });
+      });
+    });
+    describe("when JavaScript is not available", () => {
+      const setup = (example = examples.default) =>
+        renderDOM("header", example, {
+          jsEnabled: false,
+        });
 
-      describe('the menu items', () => {
-        it('clicking menu link should change the window location (mobile)', async () => {
-          setViewportWidth(320)
+      describe("the menu items", () => {
+        it("clicking menu link should change the window location (mobile)", async () => {
+          setViewportWidth(320);
 
-          const target = setup()
-          const user = userEvent.setup()
-          const link = target.querySelector('.lbcamden-header__navigation-item-link')
+          const target = setup();
+          const user = userEvent.setup();
+          const link = target.querySelector(
+            ".lbcamden-header__navigation-item-link",
+          );
 
-          const initialState = window.location.href
-          await user.click(link)
+          const initialState = window.location.href;
+          await user.click(link);
 
-          expect(initialState).not.toEqual(window.location.href)
-        })
+          expect(initialState).not.toEqual(window.location.href);
+        });
 
-        it('clicking menu link should change the window location (desktop)', async () => {
-          setViewportWidth(1024)
+        it("clicking menu link should change the window location (desktop)", async () => {
+          setViewportWidth(1024);
 
-          const target = setup()
-          const user = userEvent.setup()
-          const link = target.querySelector('.lbcamden-header__navigation-item-link')
+          const target = setup();
+          const user = userEvent.setup();
+          const link = target.querySelector(
+            ".lbcamden-header__navigation-item-link",
+          );
 
-          const initialState = window.location.href
-          await user.click(link)
+          const initialState = window.location.href;
+          await user.click(link);
 
-          expect(initialState).not.toEqual(window.location.href)
-        })
-      })
+          expect(initialState).not.toEqual(window.location.href);
+        });
+      });
 
-      describe('the search function', () => {
-        it('clicking search link should change the window location (mobile)', async () => {
-          setViewportWidth(320)
-          const target = setup()
-          const user = userEvent.setup()
-          const link = target.querySelector('.lbcamden-header__search-item-link')
-          link.href = '#search'
+      describe("the search function", () => {
+        it("clicking search link should change the window location (mobile)", async () => {
+          setViewportWidth(320);
+          const target = setup();
+          const user = userEvent.setup();
+          const link = target.querySelector(
+            ".lbcamden-header__search-item-link",
+          );
+          link.href = "#search";
 
-          const initialState = window.location.href
-          await user.click(link)
+          const initialState = window.location.href;
+          await user.click(link);
 
-          expect(initialState).not.toEqual(window.location.href)
-        })
+          expect(initialState).not.toEqual(window.location.href);
+        });
 
-        it('clicking search link should change the window location (desktop)', async () => {
-          setViewportWidth(1024)
-          const target = setup()
-          const user = userEvent.setup()
-          const link = target.querySelector('.lbcamden-header__search-item-link')
-          link.href = '#search'
+        it("clicking search link should change the window location (desktop)", async () => {
+          setViewportWidth(1024);
+          const target = setup();
+          const user = userEvent.setup();
+          const link = target.querySelector(
+            ".lbcamden-header__search-item-link",
+          );
+          link.href = "#search";
 
-          const initialState = window.location.href
-          await user.click(link)
+          const initialState = window.location.href;
+          await user.click(link);
 
-          expect(initialState).not.toEqual(window.location.href)
-        })
+          expect(initialState).not.toEqual(window.location.href);
+        });
 
-        it('the search container should not be hidden', async () => {
-          const target = setup()
-          expect(target.querySelector('#super-search-menu').hasAttribute('hidden')).toEqual(false)
-        })
-      })
-    })
-  })
-})
+        it("the search container should not be hidden", async () => {
+          const target = setup();
+          expect(
+            target.querySelector("#super-search-menu").hasAttribute("hidden"),
+          ).toEqual(false);
+        });
+      });
+    });
+  });
+});
