c8b8c74ba3d886a3a7cd6da368ccff7a037831aa
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2026-07-22 12:05:35 +0100

    rename component, change spacing

--- src/lbcamden/components/link-columns/template.test.js
+++ src/lbcamden/components/link-columns/template.test.js
@@ -9,19 +9,19 @@ const axe = configureAxe({
 
 const { render, getExamples } = require('../../../../lib/jest-helpers')
 
-const examples = getExamples('two-column-gallery')
+const examples = getExamples('link-columns')
 
-describe('two-column-gallery', () => {
+describe('link-columns', () => {
   describe('default example', () => {
     it('passes accessibility tests', async () => {
-      const $ = render('two-column-gallery', examples.default)
+      const $ = render('link-columns', examples.default)
 
       const results = await axe($.html())
       expect(results).toHaveNoViolations()
     })
 
     it('renders contents', () => {
-      const $ = render('two-column-gallery', examples.default)
+      const $ = render('link-columns', examples.default)
 
       expect($('h3').first().text()).toContain('Business rates')
       expect($('a').first().text()).toContain('Business rates explained')
@@ -33,16 +33,16 @@ describe('two-column-gallery', () => {
 
   describe('custom options', () => {
     it('renders classes', () => {
-      const $ = render('two-column-gallery', examples.classes)
+      const $ = render('link-columns', examples.classes)
 
-      const $component = $('.lbcamden-two-column-gallery')
-      expect($component.hasClass('app-two-column-gallery--custom-modifier')).toBeTruthy()
+      const $component = $('.lbcamden-link-columns')
+      expect($component.hasClass('app-link-columns--custom-modifier')).toBeTruthy()
     })
   })
 
   describe('with heading', () => {
     it('renders heading', () => {
-      const $ = render('two-column-gallery', examples['with heading and description'])
+      const $ = render('link-columns', examples['with heading and description'])
 
       const $component = $('h2')
       expect($component.text()).toContain('Contents')
