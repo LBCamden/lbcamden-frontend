c6a71958240e0e8fde104fe5f1c50be5934a41cd
Alex Wybraniec <alex.wybraniec@camden.gov.uk>
2026-08-11 20:29:49 +0100

    Release 1.0.9 (#266)
    * Release 1.0.9
    
    * add ability to add user logo to top level menu in header nav (#267)
    
    * initial
    
    * update mobile logo
    
    * fix mobile alignment
    
    * make test pass
    
    * SVG focus amend (#269)
    
    * AI assisted fix for the SVG colour change on focus. Needs review and we need to check that the new asset icon svg is correctly bundled in the package by the build scripts
    
    * remove comments
    
    * inline svgs
    
    * remove icon from assets
    
    ---------
    
    Co-authored-by: Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
    
    * change icon component tests
    
    * fix bug
    
    ---------
    
    Co-authored-by: Alex Wybraniec <alex.wybraniec@camden.gov.uk>
    
    * Ds 1099 investigate two column use on account services page (#268)
    
    * initial
    
    * implementation
    
    * rename component, change spacing
    
    * missed two files renaming
    
    * Build package
    
    ---------
    
    Co-authored-by: Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>

---
+++ src/lbcamden/components/link-columns/template.test.js
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest'
+import { configureAxe } from "vitest-axe";
+const axe = configureAxe({
+  rules: {
+    'skip-link': { enabled: false },
+    region: { enabled: false }
+  }
+})
+
+const { render, getExamples } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('link-columns')
+
+describe('link-columns', () => {
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('link-columns', examples.default)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+
+    it('renders contents', () => {
+      const $ = render('link-columns', examples.default)
+
+      expect($('h3').first().text()).toContain('Business rates')
+      expect($('a').first().text()).toContain('Business rates explained')
+      expect($('a').first().attr('href')).toEqual('#')
+      expect($('.lbcamden-link--action').first().text()).toContain('View all')
+      expect($('.lbcamden-link--action').first().attr('href')).toEqual('#')
+    })
+  })
+
+  describe('with heading', () => {
+    it('renders heading', () => {
+      const $ = render('link-columns', examples['with heading and description'])
+
+      const $component = $('h2')
+      expect($component.text()).toContain('Contents')
+    })
+  })
+})
