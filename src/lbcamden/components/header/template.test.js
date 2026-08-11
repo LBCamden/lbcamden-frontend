6cea0491fa05ae0002e6745d68e0c49f4d8c0c29
Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
2026-08-11 17:12:41 +0100

    add ability to add user logo to top level menu in header nav (#267)
    * initial
    
    * update mobile logo
    
    * fix mobile alignment
    
    * make test pass
    
    * SVG focus amend (#269)
    
    * AI assisted fix for the SVG colour change on focus. Needs review and we need to check that the new asset icon svg is correctly bundled in the package by the build scripts
    
    * remove comments
    
    * inline svgs
    
    * remove icon from assets
    
    ---------
    
    Co-authored-by: Olu Niyi-Awosusi <oluwatomisin.niyi-awosusi@camden.gov.uk>
    
    * change icon component tests
    
    * fix bug
    
    ---------
    
    Co-authored-by: Alex Wybraniec <alex.wybraniec@camden.gov.uk>

--- src/lbcamden/components/header/template.test.js
+++ src/lbcamden/components/header/template.test.js
@@ -57,5 +57,17 @@ describe('header', () => {
       const $threeColumnLayout = $('.lbcamden-header__navigation-second-items--full li')
       expect($threeColumnLayout).toBeTruthy()
     })
+
+    it('renders navigation item attributes correctly', () => {
+      const $ = render('header', examples['Standard application with icon on one navigation item'])
+
+      const $links = $('.lbcamden-header__navigation-item-link')
+      expect($links.length).toEqual(3)
+      expect($links.eq(2).text().trim()).toEqual('Account')
+      expect($links.eq(2).attr('href')).toEqual('#3')
+
+      const $accountIcon = $links.eq(2).find('.lbcamden-header__navigation-item-link-icon[data-icon="user"]')
+      expect($accountIcon.length).toEqual(1)
+    })
   })
 })
